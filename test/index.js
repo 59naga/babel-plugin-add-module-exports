d5d5bce92115d58cf698ab49813f0b92ae4751ca
59naga <i59naga@icloud.com>
2015-11-13 21:19:53 +0900

    Code refactor (ref #4)

--- test/index.js
+++ test/index.js
@@ -1,65 +1,71 @@
+/* eslint eqeqeq: 1 */
+
 // Dependencies
-var babel= require('babel-core')
-var assert= require('power-assert')
+var babel = require('babel-core')
+var assert = require('power-assert')
+var fsExtra = require('fs-extra')
+var rimraf = require('rimraf')
 
 // Fixture
-var fixture=`
+var fixture = `
 let foo= 'bar'
 export default 'baz'
 export {foo}
 `
+var fixturePath = 'node_modules/fixture'
 
 // Specs
-describe('babel-plugin-add-module-exports',function(){
-  // wait for the babel-preset-es2015
-  this.timeout(5000)
-  before(()=>{
-    babel.transform('',{
-      presets: ['es2015'],
-      plugins: ['transform-es2015-modules-commonjs'],
-    })
+describe('babel-plugin-add-module-exports', function () {
+  this.timeout(5000) // wait for load the `babel-preset-es2015`
+
+  beforeEach(() => {
+    try {
+      rimraf.sync(fixturePath)
+      delete require.cache[require.resolve('fixture')]
+    } catch (e) {
+
+    }
   })
 
-  it('Nope.',()=>{
-    var result= babel.transform(fixture,{
+  it('Nope.', () => {
+    var result = babel.transform(fixture, {
       presets: ['es2015'],
-      plugins: ['transform-es2015-modules-commonjs'],
+      plugins: ['transform-es2015-modules-commonjs']
     })
+    fsExtra.outputFileSync(fixturePath + '/index.js', result.code)
 
-    var EOFExpression= result.ast.program.body.slice(-1)[0].expression
-    assert(EOFExpression.left.object.name !== 'module')
-    assert(EOFExpression.left.property.name !== 'exports')
+    var object = require('fixture')
+    assert(object.default === 'baz')
+    assert(object.foo === 'bar')
   })
 
-  it('Add the `module.exports = Object.assign(exports.default,exports);` to EOF.',()=>{
-    var result= babel.transform(fixture,{
+  it('Add the `module.exports = Object.assign(exports.default,exports);` to EOF.', () => {
+    var result = babel.transform(fixture, {
       presets: ['es2015'],
-      plugins: ['../lib/index.js'],
+      plugins: ['../lib/index.js']
     })
+    fsExtra.outputFileSync(fixturePath + '/index.js', result.code)
 
-    var EOFExpression= result.ast.program.body.slice(-1)[0].expression
-    assert(EOFExpression.left.object.name === 'module')
-    assert(EOFExpression.left.property.name === 'exports')
-    assert(EOFExpression.operator === '=')
-    assert(EOFExpression.right.callee.object.name === 'Object')
-    assert(EOFExpression.right.callee.property.name === 'assign')
+    var object = require('fixture')
+    assert(object == 'baz')// not '==='
+    assert(object.default === 'baz')
+    assert(object.foo === 'bar')
   })
 
-  it('issue#1',()=>{
-    var result= babel.transform(fixture,{
+  it('issue#1', () => {
+    var result = babel.transform(fixture, {
       presets: ['es2015'],
       plugins: [
         '../lib/index.js',
         '../lib/index.js',
-        '../lib/index.js',
-      ],
+        '../lib/index.js'
+      ]
     })
+    fsExtra.outputFileSync(fixturePath + '/index.js', result.code)
 
-    var EOFExpression= result.ast.program.body.slice(-1)[0].expression
-    assert(EOFExpression.left.object.name === 'module')
-    assert(EOFExpression.left.property.name === 'exports')
-    assert(EOFExpression.operator === '=')
-    assert(EOFExpression.right.callee.object.name === 'Object')
-    assert(EOFExpression.right.callee.property.name === 'assign')
+    var object = require('fixture')
+    assert(object == 'baz')// not '==='
+    assert(object.default === 'baz')
+    assert(object.foo === 'bar')
   })
 })
