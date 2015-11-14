fc69edcdc38afe87f35509dae286e63f6e59573f
Junle Li <lijunle@gmail.com>
2015-11-14 18:37:47 +0800

    Add the test specs.
    - Defines the code and its expected module exports result.
    - Even the wrong behavior in Babel@5 should be compatible.

---
+++ test/spec.js
@@ -0,0 +1,97 @@
+module.exports = [
+  {
+    name: 'export default to module.exports if only export default',
+    code: 'export default "default-entry"',
+    expected: {
+      module: 'default-entry',
+      exports: {
+        default: 'default-entry'
+      }
+    }
+  },
+  {
+    name: 'export other entries to module.exports if no default entry',
+    code: 'export const other1 = "entry1"; export const other2 = "entry2"',
+    expected: {
+      module: {
+        other1: 'entry1',
+        other2: 'entry2'
+      },
+      exports: {
+        other1: 'entry1',
+        other2: 'entry2'
+      }
+    }
+  },
+  {
+    name: 'not export default to module.exports if export multiple entries',
+    code: 'export default "default-entry"; export const other = "other-entry"',
+    expected: {
+      module: {
+        default: 'default-entry',
+        other: 'other-entry'
+      },
+      exports: {
+        default: 'default-entry',
+        other: 'other-entry'
+      }
+    }
+  },
+  {
+    name: 'export a function as default entry',
+    code: 'export default () => "default-entry"',
+    expected: {
+      module: () => 'default-entry',
+      exports: {
+        default: () => 'default-entry'
+      }
+    }
+  },
+  {
+    name: 'export default function entry with other entries',
+    code: 'export default () => "default-entry"; export const other = "other-entry"',
+    expected: {
+      module: {
+        default: () => 'default-entry',
+        other: 'other-entry'
+      },
+      exports: {
+        default: () => 'default-entry',
+        other: 'other-entry'
+      }
+    }
+  },
+  {
+    name: 'not override default object with other export entries',
+    code: 'export default { value: 1 }; export const value = 2',
+    expected: {
+      module: {
+        default: { value: 1 },
+        value: 2
+      },
+      exports: {
+        default: { value: 1 },
+        value: 2
+      }
+    }
+  },
+  {
+    // even be compatible with wrong behavior: https://github.com/babel/babel/issues/2212#issuecomment-131110500
+    name: 'allow the wrong default export as like in Babel 5',
+    code: 'export default { name: "test", version: "0.0.1" }',
+    expected: {
+      module: {
+        name: 'test',
+        version: '0.0.1',
+        default: { inspect: () => '[Circular]' }
+      },
+      exports: {
+        default: {
+          name: 'test',
+          version: '0.0.1',
+          default: { inspect: () => '[Circular]' }
+        }
+      }
+    }
+  }
+]
